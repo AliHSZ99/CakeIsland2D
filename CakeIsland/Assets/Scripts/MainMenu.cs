645d356d9c4fb26e66b1616693c5d4cda82edbb6
Ali <alihsz2002@gmail.com>
2022-05-17 00:26:08 -0400

    Created all scenes that we will have in game. Created all UI (Bearbone meaning there is no design but the buttons work), and did the flow of the application between different screens

---
+++ CakeIsland/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Loads the tutorial level scene
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("TutorialLevel");
+    }
+
+    // Exits the game no matter which scene it is in
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    // Brings the user to the settings page
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    // Brings the user back to the main menu
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // Brings the user to the first level
+    public void Level1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    // Brings the user to the second level
+    public void Level2() {
+        SceneManager.LoadScene("Level2");
+    }
+
+    // Brings the user to the the final boss level
+    public void BossLevel() {
+        SceneManager.LoadScene("BossLevel");
+    }
+
+    public void WinScreen() {
+        SceneManager.LoadScene("WinScreen");
+    }
+
+    // Method that lets the player try again. 
+    // Here, we are supposed to check which level the player is on and check if they player has enough coins.
+    public void TryAgain() {
+
+    }
+
+}
