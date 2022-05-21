17b4cb93b6352b1068bd980fafa42caa845417af
Christian Kfoury <christian_kfoury558@hotmail.ca>
2022-05-21 16:40:16 -0400

    Added angles to slope ray casting Finished level 2 map design Added checkpoints
    Co-authored-by: Ali Zoubeidi <Ali Zoubeidi @alihsz2002@gmail.com>

--- CakeIsland/Assets/Scripts/GroundCheck.cs
+++ CakeIsland/Assets/Scripts/GroundCheck.cs
@@ -26,14 +26,46 @@ public class GroundCheck : MonoBehaviour
 
     private void GroundCheckMethod()
     {
-        Hit2DRight = Physics2D.Raycast(rayCastOrigin.position, Vector2.right, 2f, layerMask);
-        Hit2DLeft = Physics2D.Raycast(rayCastOrigin.position, Vector2.left, 2f, layerMask);
+        Hit2DRight = Physics2D.Raycast(rayCastOrigin.position, Vector2.right, 0.85f, layerMask);
+        Hit2DLeft = Physics2D.Raycast(rayCastOrigin.position, Vector2.left, 0.85f, layerMask);
         Debug.DrawRay(rayCastOrigin.position, Vector2.right, Color.blue);
         if (Hit2DRight != false || Hit2DLeft != false)
         {
-            rigidBody.velocity = Vector3.zero;
-            rigidBody.angularVelocity = 0f;
-            Debug.DrawRay(rayCastOrigin.position, Vector2.right, Color.red);
+            float DISTANCE_FEET_TOP = Vector2.Distance(new Vector2(rayCastOrigin.position.x, rayCastOrigin.position.y),
+                    new Vector2(playerFeet.position.x, playerFeet.position.y));
+            float angleInDegrees = 0f;
+            if (PlayerController.facingRight) {
+                if (Hit2DRight != false)
+                {
+                    float DISTANCE_FEET_RAYCAST = Vector2.Distance(new Vector2(playerFeet.position.x, playerFeet.position.y),
+                        new Vector2(Hit2DRight.point.x, Hit2DRight.point.y));
+                    float angle = Mathf.Asin(DISTANCE_FEET_TOP / DISTANCE_FEET_RAYCAST);
+                    angleInDegrees = (360 / (2 * Mathf.PI)) * angle;
+                    /*float angle = Vector2.Angle(new Vector2(playerFeet.position.x, playerFeet.position.y), new Vector2(playerFeet.position.x, Hit2DRight.distance));*/
+                }
+            }
+            
+            if (!PlayerController.facingRight)
+            {
+                if (Hit2DLeft != false)
+                {
+                    float DISTANCE_FEET_RAYCAST = Vector2.Distance(new Vector2(playerFeet.position.x, playerFeet.position.y),
+                        new Vector2(Hit2DLeft.point.x, Hit2DLeft.point.y));
+                    float angle = Mathf.Asin(DISTANCE_FEET_TOP / DISTANCE_FEET_RAYCAST);
+                    angleInDegrees = (360 / (2 * Mathf.PI)) * angle;
+                    /*float angle = Vector2.Angle(new Vector2(playerFeet.position.x, playerFeet.position.y), new Vector2(playerFeet.position.x, Hit2DRight.distance));*/
+                }
+            }
+
+
+            Debug.Log("Angle is " + angleInDegrees);
+
+            if (angleInDegrees >= 30f && angleInDegrees <= 45f)
+            {
+                rigidBody.velocity = Vector3.zero;
+                rigidBody.angularVelocity = 0f;
+                Debug.DrawRay(rayCastOrigin.position, Vector2.right, Color.red);
+            }
         }
 
     }
